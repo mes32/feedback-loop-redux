96765b7271cc0cd0338e9e72146217b06561e257
mes32 <stockman.mike@gmail.com>
2019-01-26 13:01:55 -0600

    Submit user feeback from Review.js (via POST /prime-feeback).

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,27 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// This component allows the user to review their feeback before submitting it
+// to the server.
 class Review extends Component {
 
-    renderSubmitButton() {
-        const complete = this.isComplete();
-        if (complete) {
-            return (<button>Submit</button>);
+    // If the rating has not yet been set, returns a placeholder character
+    ratingOrSpacer(rating) {
+        if (rating) {
+            return rating;
+        } else {
+            return '_';
+        }
+    }
+
+    // Builds a 'Submit' button for the current feeback dataset. Changes button 
+    // to enabled with different text when the feeback is complete and ready to
+    // POST to the server.
+    submitButton() {
+        const ready = this.readyToSubmit();
+        if (ready) {
+            return (<button onClick={this.submit}>Submit</button>);
         } else {
             return (<button disabled>Incomplete</button>);
         } 
@@ -14,7 +29,7 @@ class Review extends Component {
 
     // Returns true if all required fields are filled out and this dataset is 
     // ready to submit to the server.
-    isComplete() {
+    readyToSubmit() {
         const feelings = this.props.rs.feelingsRating;
         const understanding = this.props.rs.understandingRating;
         const support = this.props.rs.supportRating;
@@ -24,16 +39,43 @@ class Review extends Component {
         return false;
     }
 
+    // Sends the user's feedback to the server (via POST /prime-feeback)
+    submit = (event) => {
+        const feeback = {
+            feeling: this.props.rs.feelingsRating,
+            understanding: this.props.rs.understandingRating,
+            support: this.props.rs.supportRating,
+            comments: this.props.rs.comments,
+        };
+        axios({
+            method: 'POST',
+            url: '/prime-feeback',
+            data: feeback,
+        }).then((response) => {
+            console.log(response);
+            // TODO: Send browser to confirmation/thank-you page url
+            // this.props.history.push('/thank-you');
+        }).catch((error) => {
+            // TODO: Optimize error display for users
+            const errorMessage = `Server error: ${error}`;
+            alert(errorMessage);
+            console.log(errorMessage);
+        });
+    }
+
     // Display component on page
     render() {
+        const feelings = this.ratingOrSpacer(this.props.rs.feelingsRating);
+        const understanding = this.ratingOrSpacer(this.props.rs.understandingRating);
+        const support = this.ratingOrSpacer(this.props.rs.supportRating);
         return (
             <div>
                 <h2>Review Your Feedback</h2>
-                <p>Feelings: {this.props.rs.feelingsRating}</p>
-                <p>Understanding: {this.props.rs.understandingRating}</p>
-                <p>Support: {this.props.rs.supportRating}</p>
+                <p>Feelings: {feelings}</p>
+                <p>Understanding: {understanding}</p>
+                <p>Support: {support}</p>
                 <p>Comments: {this.props.rs.comments}</p>
-                {this.renderSubmitButton()}
+                {this.submitButton()}
             </div>
         );
     }
